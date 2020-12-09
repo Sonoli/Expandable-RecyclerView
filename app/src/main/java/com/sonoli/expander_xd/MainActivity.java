5cc24219ad86f9033dbeca5c74e88553a48d4717
Sonoli <mistet.sonoli@gmail.com>
2020-12-09 20:17:38 +0300

    expandable list, users and post class add

--- app/src/main/java/com/sonoli/expander_xd/MainActivity.java
+++ app/src/main/java/com/sonoli/expander_xd/MainActivity.java
@@ -9,14 +9,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
     ExpandableListView expandableListView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         expandableListView = findViewById(R.id.expandableListView);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+      //  Call<List<Users>> call = jsonPlaceHolderApi.getUsers();
 
         HashMap<String, List<String>> item = new HashMap<>();
         ArrayList<String> linuxGroups = new ArrayList<>();
